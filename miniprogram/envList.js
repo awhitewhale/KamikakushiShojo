fc4567f533a0286ecd5de46f4ddc93fa33e4e8e9
awhitewhale <45808486+awhitewhale@users.noreply.github.com>
2022-04-21 14:52:54 +0800

    Update envList.js

--- miniprogram/envList.js
+++ miniprogram/envList.js
@@ -1,6 +1,5 @@
-const envList = [{"envId":"prod-9g4dpsdo0c4bcb2f","alias":"cloud1"}]
-const isMac = false
 module.exports = {
-    envList,
-    isMac
+  envList: [{
+    envId:'prod-9g4dpsdo0c4bcb2f'
+  }]
 }
